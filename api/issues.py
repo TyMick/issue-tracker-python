1b712f116bc153f6dd706d3ca3b2582873cdc553
Ty Mick <ty@tymick.me>
2020-09-30 20:31:33 -0400

    Add add_new_issue

--- api/issues.py
+++ api/issues.py
@@ -1,9 +1,71 @@
+from flask import request
+from database import get_db
+from datetime import datetime, timezone
+import nanoid
+
+
 def get_issues(project_id):
     pass
 
 
 def add_new_issue(project_id):
-    pass
+    if (
+        not request.form.get("issue_title")
+        or not request.form.get("issue_text")
+        or not request.form.get("created_by")
+    ):
+        return "Missing required inputs"
+
+    issue_id = nanoid.generate()
+
+    try:
+        db = get_db()
+        c = db.cursor()
+        c.execute(
+            """
+            INSERT INTO issue(
+                project_id,
+                _id,
+                issue_title,
+                issue_text,
+                created_by,
+                assigned_to,
+                status_text
+            ) VALUES(?, ?, ?, ?, ?, ?, ?)
+            """,
+            (
+                project_id,
+                issue_id,
+                request.form.get("issue_title", ""),
+                request.form.get("issue_text", ""),
+                request.form.get("created_by", ""),
+                request.form.get("assigned_to", ""),
+                request.form.get("status_text", ""),
+            ),
+        )
+        db.commit()
+
+        c.execute(
+            """
+            SELECT
+                _id,
+                issue_title,
+                issue_text,
+                created_by,
+                assigned_to,
+                status_text,
+                strftime("%Y-%m-%dT%H:%M:%SZ", created_on) AS created_on,
+                strftime("%Y-%m-%dT%H:%M:%SZ", updated_on) AS updated_on,
+                open
+            FROM issue
+            WHERE project_id == ? AND _id == ?
+            """,
+            (project_id, issue_id),
+        )
+        return c.fetchone()
+
+    except:
+        return "Database error"
 
 
 def update_issue(project_id):
---
+++ database.py
@@ -0,0 +1,54 @@
+from flask import g
+import sqlite3
+
+DATABASE = "issues.db"
+
+
+def dict_factory(cursor, row):
+    """
+    Turns rows into dictionaries (and corrects boolean values) for easier JSON
+    conversion. Plugs into Connection.row_factory.
+    """
+
+    BOOLEAN_COLUMNS = {"open"}
+
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        key = col[0]
+        value = bool(row[idx]) if key in BOOLEAN_COLUMNS else row[idx]
+        d[key] = value
+
+    return d
+
+
+def get_db():
+    db = getattr(g, "_database", None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+        db.row_factory = dict_factory
+    return db
+
+
+def init_db():
+    db = get_db()
+    c = db.cursor()
+    c.executescript(
+        """
+        CREATE TABLE IF NOT EXISTS issue(
+            project_id TEXT NOT NULL,
+            _id TEXT PRIMARY KEY,
+            issue_title TEXT NOT NULL,
+            issue_text TEXT NOT NULL,
+            created_by TEXT NOT NULL,
+            assigned_to TEXT NOT NULL DEFAULT "",
+            status_text TEXT NOT NULL DEFAULT "",
+            created_on DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            updated_on DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            open BOOLEAN NOT NULL DEFAULT TRUE
+        ) WITHOUT ROWID;
+
+        CREATE UNIQUE INDEX IF NOT EXISTS thread_idx ON issue(project_id, _id);
+        """
+        # Add indices
+    )
+    db.commit()
